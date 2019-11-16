95328bf3cd674098fc3f71c7525ec598d3474d80
Pradyumna Mahajan <pradyumnamahajan@protonmail.com>
2019-11-16 15:55:30 +0530

    port fix

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ const getData = async (itemsRequested) => {
 
 }
 
-let port = server.listen(process.env.PORT || 3000);
+let port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 
